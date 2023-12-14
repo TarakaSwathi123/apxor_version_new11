06f29d183095763ad897126d5ec1a6b3cec55eb8
Taraka Swathi <73474970+TarakaSwathi123@users.noreply.github.com>
2023-12-14 17:45:43 +0530

    Update init.js

--- init.js
+++ init.js
@@ -22,9 +22,6 @@
       };
     });
    })(window, document, "script");
-     
- 
- 
     Apxor.init(
       "b0bf1fc7-b104-4e92-9cc5-590fcb685c29",
       {
@@ -32,13 +29,9 @@
         plugins: ["ApxorRTM"],
         version: "144",
       },
-      
       function success(data) {
         console.log("APXOR SDK Initialized");
       },
       function error() {
         console.log("APXOR SDK not initialized");
       });
- 
-   
-
