afacb876f5bd831edad71caf7fb8a10d0c8b2e54
Taraka Swathi <73474970+TarakaSwathi123@users.noreply.github.com>
2023-12-14 17:52:36 +0530

    Update initinit.js

--- initinit.js
+++ initinit.js
@@ -22,7 +22,7 @@ const _d = new Date();
       };
     });
    })(window, document, "script");
-    Apxor.init(
+    window.Apxor.init(
       "b0bf1fc7-b104-4e92-9cc5-590fcb685c29",
       {
         idle_time_out: 1800,
