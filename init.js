5b97d31153c6052acedf85b74a9cce14d4090b9f
Taraka Swathi <73474970+TarakaSwathi123@users.noreply.github.com>
2023-12-14 17:37:17 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,21 +1,44 @@
 console.log("Initializing Apxor");
-window.Apxor?.init(
+ 
+  const _d = new Date();
+  (function (a, p, x, o, r) {
+    Apxor = a.Apxor || { _q: [], _st: _d };
+    [
+      "init",
+      "setUserId",
+      "setUserProperties",
+      "setSessionProperties",
+      "logPageView",
+      "logEvent",
+      "logClientEvent",
+      "setAppVersion",
+      "getClientId",
+      "getSessionId",
+      "startNewSession",
+      "endSession",
+      "flattenJSON",
+      "setRedirectionHandler",
+    ].forEach(function (m) {
+      Apxor[m] = function () {
+        this._q.push({ m: m, args: arguments });
+      };
+    });
+   })(window, document, "script");
+ 
+ 
+ window.Apxor.init(
       "b0bf1fc7-b104-4e92-9cc5-590fcb685c29",
       {
-        debug: true,
-        idle_time_out: 600,
+        idle_time_out: 1800,
         plugins: ["ApxorRTM"],
         version: "144",
-        events_upload_time: 5,
-        retry_time: 5,
-        bulk_upload: true,
-        ed_t_time: 90,
-        level: "debug",
       },
+      
       function success(data) {
-        console.log("SDK Initialized: " + data.client_id);
+        console.log("APXOR SDK Initialized");
       },
       function error() {
-        console.log("SDK is not initialized.");
-      }
-    );
+        console.log("APXOR SDK not initialized");
+      });
+   
+
